393d82edd22a178b76cf8164d80089748d4bfd1d
Mikhail Morozov <mixail.mo95@mail.ru>
2024-10-21 14:42:40 +0300

    add

--- games/game-even.js
+++ games/game-even.js
@@ -8,7 +8,7 @@ export default function isEven() {
   while (correctAnswer < 3) {
     correctAnswer += 1;
     const num = getRandomNum(1, 100);
-    console.log('Question:', num);
+    console.log('Question:', num.toString());
     const answer = readlineSync.question('Your answer: ');
     const result = num % 2 === 0 ? 'yes' : 'no';
     getAnswer(correctAnswer, answer, result, name);
