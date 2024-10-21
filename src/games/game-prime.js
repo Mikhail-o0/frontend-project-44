7df320b966c20fe63f6320e84e060d0ac7bda319
Mikhail Morozov <mixail.mo95@mail.ru>
2024-10-21 17:10:05 +0300

    moved the "games" directory to "src"

---
+++ src/games/game-prime.js
@@ -0,0 +1,21 @@
+import readlineSync from 'readline-sync';
+import {
+  getRandomNum, toGreet, getAnswer, getPrime,
+} from '../index.js';
+
+export default function getPrimeNumber() {
+  const name = toGreet();
+  console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
+  let correctAnswer = 0;
+  while (correctAnswer < 3) {
+    correctAnswer += 1;
+    const num = getRandomNum(2, 50);
+    const result = getPrime(num);
+    console.log('Question:', num);
+    const answer = readlineSync.question('Your answer: ');
+    getAnswer(correctAnswer, answer, result, name);
+    if (answer !== result) {
+      return;
+    }
+  }
+}
---
+++ src/games/game-progression.js
@@ -0,0 +1,23 @@
+import readlineSync from 'readline-sync';
+import {
+  getRandomNum, toGreet, getRandomProgression, getAnswer,
+} from '../index.js';
+
+export default function getProgression() {
+  const name = toGreet();
+  console.log('What number is missing in the progression?');
+  let correctAnswer = 0;
+  while (correctAnswer < 3) {
+    correctAnswer += 1;
+    const progression = getRandomProgression();
+    const randomIndex = getRandomNum(0, progression.length - 1);
+    const result = progression[randomIndex];
+    progression.splice(randomIndex, 1, '..');
+    console.log('Question:', progression.join(' '));
+    const answer = readlineSync.question('Your answer: ');
+    getAnswer(correctAnswer, Number(answer), result, name);
+    if (Number(answer) !== result) {
+      return;
+    }
+  }
+}
